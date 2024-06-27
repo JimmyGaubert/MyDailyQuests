a6ebbc134ccff495c209c38a1d4dcc00b0af8496
Earlam <63388059+JimmyGaubert@users.noreply.github.com>
2024-06-27 20:52:22 +0200

    Add files via upload

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,35 +1,81 @@
 const { Events } = require('discord.js');
 const mysql = require('mysql');
+
 module.exports = {
     name: Events.MessageCreate,
     once: false,
-    execute(message) {
-        //console.log(message.content)
-        const db = mysql.createConnection({host:`${process.env.DB_HOST}`,user:`${process.env.DB_USER}`,password:`${process.env.DB_PWD}`,database:`${process.env.DB_NAME}`});
-        db.query(`SELECT * FROM player WHERE discord_id = ${message.author.id}`, (err, results) => {
-            if (err) { throw err };
-            if (results.length === 0) {
-                db.query(`INSERT INTO player (discord_id) VALUES ("${message.author.id}")`, err => {
-                    if (err) { throw err };
-                    db.query(`INSERT INTO player_inventory (discord_id) VALUES ("${message.author.id}")`, err => {
-                        if (err) { throw err };
-                        db.query(`INSERT INTO player_money (discord_id) VALUES ("${message.author.id}")`, err => {
-                            if (err) { throw err };
-                            db.query(`INSERT INTO player_options (discord_id) VALUES ("${message.author.id}")`, err => {
-                                if (err) { throw err };
-                                db.query(`INSERT INTO player_quests (discord_id) VALUES ("${message.author.id}")`, err => {
-                                    if (err) { throw err };
-                                    db.query(`INSERT INTO player_stats (discord_id) VALUES ("${message.author.id}")`, err => {
-                                        if (err) { throw err };
-                                        //console.log('nouveau membre parlant sur le serveur. joueur ajouté en base.')
-                                        db.end();
-                                    });
-                                });
-                            });
-                        });
+    async execute(message) {
+        const db = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PWD,
+            database: process.env.DB_NAME
+        });
+
+        try {
+            const queryAsync = (query, params) => {
+                return new Promise((resolve, reject) => {
+                    db.query(query, params, (err, results) => {
+                        if (err) return reject(err);
+                        resolve(results);
                     });
                 });
             };
-        });
+
+            const results = await queryAsync(`SELECT * FROM player WHERE discord_id = ?`, [message.author.id]);
+
+            if (results.length === 0) {
+                await queryAsync(`INSERT INTO player (discord_id) VALUES (?)`, [message.author.id]);
+                await queryAsync(`INSERT INTO player_inventory (discord_id) VALUES (?)`, [message.author.id]);
+                await queryAsync(`INSERT INTO player_money (discord_id) VALUES (?)`, [message.author.id]);
+                await queryAsync(`INSERT INTO player_options (discord_id) VALUES (?)`, [message.author.id]);
+                await queryAsync(`INSERT INTO player_quests (discord_id) VALUES (?)`, [message.author.id]);
+                await queryAsync(`INSERT INTO player_stats (discord_id) VALUES (?)`, [message.author.id]);
+            }
+
+        } catch (error) {
+            console.error(error);
+        } finally {
+            db.end((err) => {
+                if (err) console.error('Error ending the database connection:', err);
+            });
+        }
     },
 };
+
+
+// const { Events } = require('discord.js');
+// const mysql = require('mysql');
+// module.exports = {
+//     name: Events.MessageCreate,
+//     once: false,
+//     execute(message) {
+//         //console.log(message.content)
+//         const db = mysql.createConnection({host:`${process.env.DB_HOST}`,user:`${process.env.DB_USER}`,password:`${process.env.DB_PWD}`,database:`${process.env.DB_NAME}`});
+//         db.query(`SELECT * FROM player WHERE discord_id = ${message.author.id}`, (err, results) => {
+//             if (err) { throw err };
+//             if (results.length === 0) {
+//                 db.query(`INSERT INTO player (discord_id) VALUES ("${message.author.id}")`, err => {
+//                     if (err) { throw err };
+//                     db.query(`INSERT INTO player_inventory (discord_id) VALUES ("${message.author.id}")`, err => {
+//                         if (err) { throw err };
+//                         db.query(`INSERT INTO player_money (discord_id) VALUES ("${message.author.id}")`, err => {
+//                             if (err) { throw err };
+//                             db.query(`INSERT INTO player_options (discord_id) VALUES ("${message.author.id}")`, err => {
+//                                 if (err) { throw err };
+//                                 db.query(`INSERT INTO player_quests (discord_id) VALUES ("${message.author.id}")`, err => {
+//                                     if (err) { throw err };
+//                                     db.query(`INSERT INTO player_stats (discord_id) VALUES ("${message.author.id}")`, err => {
+//                                         if (err) { throw err };
+//                                         //console.log('nouveau membre parlant sur le serveur. joueur ajouté en base.')
+//                                         db.end();
+//                                     });
+//                                 });
+//                             });
+//                         });
+//                     });
+//                 });
+//             };
+//         });
+//     },
+// };
